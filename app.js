75651971442edda24e49b1ad71681c627f4ef255
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-26 14:18:43 -0500

    Get Stylus stylesheets to render

--- app.js
+++ app.js
@@ -12,29 +12,30 @@ app.use(async (ctx) => {
   let url = ctx.url
   if (url === '/') {
     ctx.body = await renderTemplate('index.pug')
+    return
+  }
+
+  let filePath = path.join(__dirname, ctx.url)
+  let text
+  try {
+    text = await readFile(filePath, 'utf8')
+  } catch (err) {
+    ctx.response.status = 404
+    ctx.body = err.message
+    return
+  }
+  let ext = path.extname(filePath)
+  if (ext === '.styl') {
+    ctx.response.type = 'css'
+    ctx.body = await renderStylesheet(text, filePath)
   } else {
-    let filePath = path.join(__dirname, ctx.url)
-    let text
-    try {
-      let text = await readFile(filePath)
-    } catch (err) {
-      ctx.response.status = 404
-      ctx.body = err.message
-      return
-    }
-    let ext = path.extname(filePath)
-    if (ext === '.styl') {
-      ctx.response.type = 'css'
-      ctx.body = await renderStylesheet(text, filePath)
-    } else {
-      ctx.response.type = 'text'
-      ctx.body = text
-    }
+    ctx.response.type = 'text'
+    ctx.body = text
   }
 })
 
 async function renderTemplate(templateFile) {
-  let text = await readFile(templateFile)
+  let text = await readFile(templateFile, 'utf8')
   return pug.render(text)
 }
 
