03536f771aeba0e8cfc162534dd2e60f9d29f037
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-26 13:39:10 -0500

    Add the ability to serve up other files

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const Koa = require('koa')
-const route = require('koa-route')
-const pug = require('pug')
+const path = require('path')
 const promisify = require('es6-promisify')
+const Koa = require('koa')
+const pug = require('pug')
 
 const app = new Koa()
 const readFile = promisify(require('fs').readFile)
 
-app.use(route.get('/', index))
 
-async function index(ctx) {
-  // ctx.response.type = 'text'
-  let text = await readFile('index.pug')
-  ctx.body = pug.render(text)
+app.use(async (ctx) => {
+  let url = ctx.url
+  if (url === '/') {
+    ctx.body = await renderTemplate('index.pug')
+  } else {
+    let filePath = path.join(__dirname, ctx.url)
+    try {
+      let text = await readFile(filePath)
+      ctx.response.type = 'text'
+      ctx.body = text
+    } catch (err) {
+      ctx.response.status = 404
+      ctx.body = `${err.message}`
+    }
+  }
+})
+
+async function renderTemplate(templateFile) {
+  let text = await readFile(templateFile)
+  return pug.render(text)
 }
 
 app.listen(8000)
