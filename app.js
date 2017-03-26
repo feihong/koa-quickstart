f4c5565eecee27e3f00f61a6ab9c43a2916ab7f1
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-26 13:22:32 -0500

    Render index.pug as html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Koa = require('koa')
 const route = require('koa-route')
-// const pug = require('pug')
+const pug = require('pug')
 const promisify = require('es6-promisify')
 
 const app = new Koa()
@@ -9,8 +9,9 @@ const readFile = promisify(require('fs').readFile)
 app.use(route.get('/', index))
 
 async function index(ctx) {
-  ctx.response.type = 'text'
-  this.body = await readFile('index.pug')
+  // ctx.response.type = 'text'
+  let text = await readFile('index.pug')
+  ctx.body = pug.render(text)
 }
 
 app.listen(8000)
