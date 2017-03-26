e393c152cdf78d8c2ec99c55d64988d9cc681916
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-26 15:56:08 -0500

    Paths are correctly mapped to index.pug files

--- app.js
+++ app.js
@@ -6,44 +6,75 @@ const pug = require('pug')
 const stylus = require('stylus')
 
 const app = new Koa()
-const readFile = promisify(fs.readFile)
-const stat = promisify(fs.stat)
+const _readFile = promisify(fs.readFile)
+const readFile = path => _readFile(path, 'utf8')
 
 app.use(async (ctx) => {
   let url = ctx.url
   if (url === '/') {
-    ctx.body = await renderTemplate('index.pug')
+    url = ''
+  }
+  let path = pathlib.join(__dirname, url)
+
+  let stats = await getStats(path)
+  if (stats === null) {
+    ctx.response.status = 404
+    ctx.body = `File not found: ${path}`
     return
   }
 
-  let path = pathlib.join(__dirname, ctx.url)
-  let text
-  try {
-    text = await readFile(path, 'utf8')
-  } catch (err) {
-    ctx.response.status = 404
-    ctx.body = err.message
+  if (stats.isDirectory()) {
+    let indexFile = pathlib.join(path, 'index.pug')
+    if (await isFile(indexFile)) {
+      ctx.body = await renderTemplate(indexFile)
+    } else {
+      ctx.response.status = 404
+      ctx.body = `${path} is a directory`
+    }
     return
   }
+
   let ext = pathlib.extname(path)
   if (ext === '.styl') {
     ctx.response.type = 'css'
-    ctx.body = await renderStylesheet(text, path)
-  } else {
-    ctx.response.type = 'text'
-    ctx.body = text
+    ctx.body = await renderStylesheet(path)
+    return
   }
+
+  ctx.response.body = await readFile(path)
 })
 
+function getStats(path) {
+  return new Promise(resolve => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        resolve(null)
+      } else {
+        resolve(stats)
+      }
+    })
+  })
+}
+
+async function isFile(path) {
+  let stats = await getStats(path)
+  return (stats === null) ? false : stats.isFile()
+}
+
 async function renderTemplate(templateFile) {
-  let text = await readFile(templateFile, 'utf8')
+  let text = await readFile(templateFile)
   return pug.render(text)
 }
 
-function renderStylesheet(text, ssFile) {
+async function renderStylesheet(stylFile) {
+  let text = await readFile(stylFile)
+  return await asyncStylus(text, stylFile)
+}
+
+function asyncStylus(text, stylFile) {
   return new Promise((resolve, reject) => {
     stylus(text)
-      .set('filename', ssFile)
+      .set('filename', stylFile)
       .render((err, css) => {
         if (err) {
           reject(err)
