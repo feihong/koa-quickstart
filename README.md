c2c471ae5191922b2b81284ceb300b05cbee8bba
Feihong Hsu <feihong.hsu@gmail.com>
2017-03-26 14:02:53 -0500

    Add renderStylesheet() function

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ yarn add koa koa-route axios
 ## Run
 
 `nodemon app.js`
+
+
+## Sources
+
+- [Koa Response API](https://github.com/koajs/koa/blob/master/docs/api/response.md)
+- [Stylus API](https://github.com/stylus/stylus/blob/dev/docs/js.md)
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const promisify = require('es6-promisify')
 const Koa = require('koa')
 const pug = require('pug')
+const stylus = require('stylus')
 
 const app = new Koa()
 const readFile = promisify(require('fs').readFile)
@@ -13,13 +14,21 @@ app.use(async (ctx) => {
     ctx.body = await renderTemplate('index.pug')
   } else {
     let filePath = path.join(__dirname, ctx.url)
+    let text
     try {
       let text = await readFile(filePath)
-      ctx.response.type = 'text'
-      ctx.body = text
     } catch (err) {
       ctx.response.status = 404
-      ctx.body = `${err.message}`
+      ctx.body = err.message
+      return
+    }
+    let ext = path.extname(filePath)
+    if (ext === '.styl') {
+      ctx.response.type = 'css'
+      ctx.body = await renderStylesheet(text, filePath)
+    } else {
+      ctx.response.type = 'text'
+      ctx.body = text
     }
   }
 })
@@ -27,6 +36,20 @@ app.use(async (ctx) => {
 async function renderTemplate(templateFile) {
   let text = await readFile(templateFile)
   return pug.render(text)
+}
+
+function renderStylesheet(text, filename) {
+  return new Promise((resolve, reject) => {
+    stylus(text)
+      .set('filename', filename)
+      .render((err, css) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(css)
+        }
+      })
+  })
 }
 
 app.listen(8000)
